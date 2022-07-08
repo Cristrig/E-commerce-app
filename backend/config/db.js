56128d850e6ab4c1b4edea636284eb07b1fd492c
Cristian Trigueros <cristiantrigueros@Cristians-MBP.lan>
2022-07-07 20:51:12 -0500

    additional commit

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,
+        useUnifiedTopology: true})
+        console.log("MongoDB connetion SUCCESS")
+    } catch (error) {
+        console.log("MongoDB connection FAIL")
+        process.exit(1)
+    };)
+}
+
+module.exports = connectDB
